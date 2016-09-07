622003d3def678b5134ed36a01e65cc470fb7a32
hb <houbo_1020@163.com>
2016-09-07 22:42:15 +0800

    add options

--- tickits/tickits.py
+++ tickits/tickits.py
@@ -1,7 +1,8 @@
 #! usr/bin/python3
-#coding:utf-8
+# coding:utf-8
 
-"""Train tickets query via command-line.
+"""
+Train tickets query via command-line.
 
 Usage:
     tickets [-gdtkz] <from> <to> <date>
@@ -14,27 +15,41 @@ Options:
     -k          快速
     -z          直达
 
+
 Example:
     tickets 北京 上海 2016-08-25
 """
+
+
 from docopt import docopt
 from prettytable import PrettyTable
 import requests
+import re
 
 
 def cre_stations():
-	url_sta = 'https://kyfw.12306.cn/otn/resources/js/framework/station_name.js?station_version=1.8955'
-	sta = requests.get(url_sta,verify=False)
-	# create stations dict
-	sta_list = [station.split('|') for station in sta.text.split('@')[1:]]
-	stations = {station[1]:station[2] for station in sta_list}
-	return stations
-	
+    url_sta = 'https://kyfw.12306.cn/otn/resources/js/framework/station_name.js?station_version=1.8955'
+    sta = requests.get(url_sta,verify=False)
+    # create stations dict
+    sta_list = [station.split('|') for station in sta.text.split('@')[1:]]
+    stations = {station[1]: station[2] for station in sta_list}
+    return stations
+
+
+def colored(color,text):
+    table = {
+        'red': '\033[91m',
+        'green': '\033[92m',
+        'nocolor': '\033[0m'
+    }
+    cv = table.get(color)
+    nc = table.get('nocolor')
+    return ''.join([cv,text,nc])
+
 
 class TrainCollection(object):
-
-    # 显示车次、出发/到达站、 出发/到达时间、历时、一等坐、二等坐、软卧、硬卧、硬座
-    header = 'train station time duration first second softsleep hardsleep hardsit'.split()
+    # 显示车次、出发/到达站、 出发/到达时间、历时、商务座、特等座、一等坐、二等坐、软卧、硬卧、硬座
+    header = '车次 出发/到达站 出发/到达时间 历时 商务座 特等座 一等座 二等座 软卧 硬卧 硬座 无座'.split()
 
     def __init__(self, rows):
         self.rows = rows
@@ -57,11 +72,15 @@ class TrainCollection(object):
                 # 车次
                 row['station_train_code'],
                 # 出发、到达站
-                '\n'.join([row['from_station_name'], row['to_station_name']]),
+                '\n'.join([colored('green', row['from_station_name']),colored('red', row['to_station_name'])]),
                 # 出发、到达时间
-                '\n'.join([row['start_time'], row['arrive_time']]),
+                '\n'.join([colored('green', row['start_time']),colored('red', row['arrive_time'])]),
                 # 历时
                 self._get_duration(row),
+                # 商务座
+                row['swz_num'],
+                # 特等座
+                row['tz_num'],
                 # 一等坐
                 row['zy_num'],
                 # 二等坐
@@ -71,7 +90,9 @@ class TrainCollection(object):
                 # 软坐
                 row['yw_num'],
                 # 硬坐
-                row['yz_num']
+                row['yz_num'],
+                # 无座
+                row['wz_num']
             ]
             yield train
 
@@ -88,22 +109,43 @@ class TrainCollection(object):
         print(pt)
 
 
-
-
 def cli():
     """command-line interface"""
     arguments = docopt(__doc__)
     from_station = cre_stations().get(arguments['<from>'])
     to_station = cre_stations().get(arguments['<to>'])
     date = arguments['<date>']
+    gaotie = arguments['-g']
+    dongche = arguments['-d']
+    tekuai = arguments['-t']
+    kuaiche = arguments['-k']
+    zhida = arguments['-z']
+    commandflag = ''
+    if gaotie:
+        commandflag += 'G'
+    if dongche:
+        commandflag += 'D'
+    if tekuai:
+        commandflag += 'T'
+    if kuaiche:
+        commandflag += 'K'
+    if zhida:
+        commandflag += 'Z'
     # create URL
     url = 'https://kyfw.12306.cn/otn/lcxxcx/query?purpose_codes=ADULT&queryDate={}&from_station={}&to_station={}'.format(
         date, from_station, to_station)
     r = requests.get(url,verify=False)
     rows = r.json()['data']['datas']
-    trains = TrainCollection(rows)
-    trains.pretty_print()
-
+    if commandflag:
+        rows = filter(lambda x:re.match(commandflag,x['station_train_code']),rows)
+    # print(rows)
+    try:
+        trains = TrainCollection(rows)
+        trains.pretty_print()
+    except ValueError:
+        print("服务器错误，请重试！")
+    except TypeError:
+        print("输入错误，请重试")
 
 
 if __name__ == '__main__':
