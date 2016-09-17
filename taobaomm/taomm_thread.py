d6511a5d0c76484ae5d538396aa2dbbc82befcb2
hb <houbo_1020@163.com>
2016-09-17 09:22:30 +0800

    edit README

--- taobaomm/taomm_thread.py
+++ taobaomm/taomm_thread.py
@@ -5,6 +5,7 @@ from selenium import webdriver
 from bs4 import BeautifulSoup
 from multiprocessing.dummy import Pool as TreadPool
 from http.client import HTTPException
+from http.client import IncompleteRead
 from socket import error as SocketError
 import urllib.request
 import re
@@ -38,7 +39,7 @@ def getperMM(MMURL,MMpath):
                 urllib.request.urlretrieve("https:"+MMimage['src'].lstrip(),MMpath+"/"+str(cnt)+MMimage['src'][-4:])
                 print(str(cnt),end=";")
                 cnt += 1
-            except (urllib.request.URLError,KeyError,HTTPException,SocketError) as e:
+            except (urllib.request.URLError,KeyError,HTTPException,IncompleteRead,SocketError) as e:
                 print(e)
                 continue
             # if cnt >5:
